467c1dc8b937e0ef91326105016c350723b745f9
xelp <slaversprojects@gmail.com>
2024-04-10 18:38:46 +0200

    Swap Nodes

--- sprint4/swap_nodes/SwapNodes.java
+++ sprint4/swap_nodes/SwapNodes.java
@@ -1,2 +1,21 @@
-package swap_nodes;public class SwapNodes {
+package swap_nodes;
+
+import add_two_numbers.ListNode;
+
+public class SwapNodes {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        firstNode.next = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        return secondNode;
+    }
+
 }
