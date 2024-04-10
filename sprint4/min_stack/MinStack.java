ed670525dc6448ac2edfeb797e61b07ef60b697c
xelp <slaversprojects@gmail.com>
2024-04-10 18:37:18 +0200

    Min Stack

--- sprint4/min_stack/MinStack.java
+++ sprint4/min_stack/MinStack.java
@@ -1,2 +1,45 @@
-package min_stack;public class MinStack {
+package min_stack;
+
+public class MinStack {
+
+    private int[] stack;
+    private int[] minStack;
+    private int top;
+
+    public MinStack() {
+        stack = new int[1];
+        minStack = new int[1];
+        top = -1;
+    }
+
+    public void push(int val) {
+        increaseArraySize();
+        stack[++top] = val;
+        if (top == 0 || val < minStack[top - 1]) {
+            minStack[top] = val;
+        } else {
+            minStack[top] = minStack[top - 1];
+        }
+    }
+
+    public void pop() {
+        top--;
+    }
+
+    public int top() {
+        return stack[top];
+    }
+
+    public int getMin() {
+        return minStack[top];
+    }
+
+    private void increaseArraySize() {
+        if (top + 1 == stack.length) {
+            int newCapacity = stack.length * 2;
+            stack = java.util.Arrays.copyOf(stack, newCapacity);
+            minStack = java.util.Arrays.copyOf(minStack, newCapacity);
+        }
+    }
+
 }
