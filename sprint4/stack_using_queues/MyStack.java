b40c7171e989ba6a281e18ed0b8d30473313758e
xelp <slaversprojects@gmail.com>
2024-04-10 18:37:49 +0200

    Stack Using Queues

--- sprint4/stack_using_queues/MyStack.java
+++ sprint4/stack_using_queues/MyStack.java
@@ -1,2 +1,38 @@
-package stack_using_queues;public class MyStack {
+package stack_using_queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        while (!queue1.isEmpty()) {
+            queue2.add(queue1.remove());
+        }
+        queue1.add(x);
+        while (!queue2.isEmpty()) {
+            queue1.add(queue2.remove());
+        }
+    }
+
+    public int pop() {
+        return queue1.remove();
+    }
+
+    public int top() {
+        return queue1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+
 }
